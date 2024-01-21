0d0f218de21f2b35b0eb8517d065011e27506207
SkaldetSkaeg <impotekh@gmail.com>
2024-01-21 09:45:38 +0700

    More sounds (#662)
    * Добавлены новые крики для ученых.
    Убран басбустящий смех санты
    
    * После опроса добавил Unisex в вариации.
    
    * Добавил звуки рычания унатхам
    
    * Добавлена эмоция бурления для Слаймолюдов
    
    * + спец смех для берета главного врача
    
    * Добавил дополнительное шипение паукам (возможно реальные звуки насекомых не такие крутые и надо искать альтернативы)
    
    * Переписал немного звуки, чтоб было чуть больше, чем выход звука из хитина
    
    * Update settings.json
    
    * Update settings.json
    
    * Update VocalSystem.cs
    
    * Хз, как оно потерялось

--- Content.Server/Speech/EntitySystems/VocalSystem.cs
+++ Content.Server/Speech/EntitySystems/VocalSystem.cs
@@ -59,14 +59,7 @@ public sealed class VocalSystem : EntitySystem
     {
         if (args.Handled || !args.Emote.Category.HasFlag(EmoteCategory.Vocal))
             return;
-
-        // snowflake case for wilhelm scream easter egg
-        if (args.Emote.ID == component.ScreamId)
-        {
-            args.Handled = TryPlayScreamSound(uid, component);
-            return;
-        }
-
+            
         // SS220 Chat-Special-Emote begin
         //Will play special emote if it exists
         if(CheckSpecialSounds(uid, component, args.Emote))
@@ -75,6 +68,14 @@ public sealed class VocalSystem : EntitySystem
             return;
         }
         // SS220 Chat-Special-Emote end
+
+        // snowflake case for wilhelm scream easter egg
+        if (args.Emote.ID == component.ScreamId)
+        {
+            args.Handled = TryPlayScreamSound(uid, component);
+            return;
+        }
+
         // just play regular sound based on emote proto
         args.Handled = _chat.TryPlayEmoteSound(uid, component.EmoteSounds, args.Emote);
     }
