3611a178fa5ad7d0317cf799a28cb162755d6bb0
SkaldetSkaeg <impotekh@gmail.com>
2024-04-02 00:17:02 +0700

    Fulls_days_tasks2 (#864)
    * +bananaflapjack craft
    
    * +new event BananaRin +CableRandomSpawRule

---
+++ Content.Server/SS220/StationEvents/CableRandomSpawnRule.cs
@@ -0,0 +1,60 @@
+using Content.Server.StationEvents.Components;
+using Content.Server.StationEvents.Events;
+using Content.Server.GameTicking.Rules.Components;
+using Content.Server.Station.Components;
+using Content.Shared.Storage;
+using Robust.Shared.Map;
+using Robust.Shared.Random;
+using Content.Server.Power.Components;
+
+namespace Content.Server.SS220.StationEvents;
+
+public sealed class CableRandomSpawnRule : StationEventSystem<CableRandomSpawnRuleComponent>
+{
+    /*
+     * DO NOT COPY PASTE THIS TO MAKE YOUR MOB EVENT.
+     * USE THE PROTOTYPE.
+     */
+
+    protected override void Started(EntityUid uid, CableRandomSpawnRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
+    {
+        base.Started(uid, component, gameRule, args);
+
+        if (!TryGetRandomStation(out var station))
+        {
+            return;
+        }
+
+        var locations = EntityQueryEnumerator<CableComponent, TransformComponent>();
+        var validLocations = new List<EntityCoordinates>();
+        while (locations.MoveNext(out _, out _, out var transform))
+        {
+            if (CompOrNull<StationMemberComponent>(transform.GridUid)?.Station == station)
+            {
+                validLocations.Add(transform.Coordinates);
+                foreach (var spawn in EntitySpawnCollection.GetSpawns(component.Entries, RobustRandom))
+                {
+                    Spawn(spawn, transform.Coordinates);
+                }
+            }
+        }
+
+        if (component.SpecialEntries.Count == 0 || validLocations.Count == 0)
+        {
+            return;
+        }
+
+        // guaranteed spawn
+        var specialEntry = RobustRandom.Pick(component.SpecialEntries);
+        var specialSpawn = RobustRandom.Pick(validLocations);
+        Spawn(specialEntry.PrototypeId, specialSpawn);
+
+        foreach (var location in validLocations)
+        {
+            foreach (var spawn in EntitySpawnCollection.GetSpawns(component.SpecialEntries, RobustRandom))
+            {
+                Spawn(spawn, location);
+            }
+        }
+    }
+}
---
+++ Content.Server/SS220/StationEvents/CableRandomSpawnRuleComponent.cs
@@ -0,0 +1,16 @@
+﻿using Content.Shared.Storage;
+
+namespace Content.Server.SS220.StationEvents;
+
+[RegisterComponent, Access(typeof(CableRandomSpawnRule))]
+public sealed partial class CableRandomSpawnRuleComponent : Component
+{
+    [DataField("entries")]
+    public List<EntitySpawnEntry> Entries = new();
+
+    /// <summary>
+    /// At least one special entry is guaranteed to spawn
+    /// </summary>
+    [DataField("specialEntries")]
+    public List<EntitySpawnEntry> SpecialEntries = new();
+}
