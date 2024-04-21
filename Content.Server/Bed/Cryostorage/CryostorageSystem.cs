8e5ed30934b0a8cabc88fbe7b339c7cccba8240b
TheArturZh <thearturzh@gmail.com>
2024-04-21 14:37:22 +0300

    fix build

--- Content.Server/Bed/Cryostorage/CryostorageSystem.cs
+++ Content.Server/Bed/Cryostorage/CryostorageSystem.cs
@@ -242,7 +242,7 @@ public sealed class CryostorageSystem : SharedCryostorageSystem
                 ("character", name),
                 ("job", CultureInfo.CurrentCulture.TextInfo.ToTitleCase(jobName))
             ), Loc.GetString("earlyleave-cryo-sender"),
-            playDefaultSound: false
+            playSound: false
         );
     }
 
