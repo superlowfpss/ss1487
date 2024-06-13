f11fffa5bb9233c0e73a25bb7294a20c169fbb25
FunTust <49007663+FunTust@users.noreply.github.com>
2024-06-13 23:31:53 +0300

    ToggleVisualLayers fix (#1124)
    * Update ClothingSystem.cs
    
    * add some comments
    
    * Update ClothingSystem.cs
    
    Co-authored-by: Dexler <69513582+DexlerXD@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Dexler <69513582+DexlerXD@users.noreply.github.com>

--- Content.Shared/Clothing/EntitySystems/ClothingSystem.cs
+++ Content.Shared/Clothing/EntitySystems/ClothingSystem.cs
@@ -98,6 +98,13 @@ public abstract class ClothingSystem : EntitySystem
                 {
                     if (comp.Slots.Contains(layer))
                     {
+                        //ss220-ToggleVisualLayers-Fix begin
+                        //FunTust:Check where the item is placed and if it is not one of the clothing slots then do not touch the visibility
+                        if (_invSystem.TryGetContainingSlot(item, out var currentItemSlot)
+                        && currentItemSlot.SlotGroup != "Default")
+                                break;
+                        //ss220-ToggleVisualLayers-Fix end
+
                         //Checks for mask toggling. TODO: Make a generic system for this
                         if (comp.HideOnToggle && TryComp(item, out MaskComponent? mask) && TryComp(item, out ClothingComponent? clothing))
                         {
