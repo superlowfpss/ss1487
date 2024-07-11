6e255bc8e0528dd71b4b980985e6c89f76e2a7d2
Anri <elyasin.ane@mail.ru>
2024-07-11 22:11:19 +0300

    Make Test To Fix Test (#1337)
    * MakeTestToFixTest
    
    * FixDatasetTest
    
    * FixNamespace

---
+++ Content.IntegrationTests/SS220/Tests/DatasetTest.cs
@@ -0,0 +1,28 @@
+using System.Data;
+using System.Linq;
+using Content.Shared.Dataset;
+using Robust.Shared.Prototypes;
+
+namespace Content.IntegrationTests.SS220.Tests;
+
+[TestFixture]
+public sealed class SS220DatasetTest
+{
+    [Test, Order(1)] // to make sure that null entry in dataset wont fail other tests
+    public async Task CheckForNullEntriesTest()
+    {
+        await using var pair = await PoolManager.GetServerClient();
+
+        var server = pair.Server;
+        var protoMan = server.ResolveDependency<IPrototypeManager>();
+
+        var protos = protoMan.EnumeratePrototypes<DatasetPrototype>().OrderBy(p => p.ID);
+
+        foreach (var proto in protos)
+            foreach (var line in proto.Values)
+                Assert.That(line is not null, $"Current dataset prototype ID is {proto.ID}");
+        // null values generates by .yml if you write " - ", to make a empty string use " - !!string"
+        // null values wont fatal until f.e. random name generator tries to use that value
+        await pair.CleanReturnAsync();
+    }
+}
