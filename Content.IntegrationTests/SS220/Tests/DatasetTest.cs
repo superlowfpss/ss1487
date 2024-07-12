6740835299444420388074dcdb04682c82463ef3
Anri <elyasin.ane@mail.ru>
2024-07-12 23:26:06 +0300

    License for new test (#1346)

--- Content.IntegrationTests/SS220/Tests/DatasetTest.cs
+++ Content.IntegrationTests/SS220/Tests/DatasetTest.cs
@@ -1,3 +1,4 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 using System.Data;
 using System.Linq;
 using Content.Shared.Dataset;
