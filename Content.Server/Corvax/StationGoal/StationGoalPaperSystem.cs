AdventureSpaceTeam/AdventureSpace

Upgrade station goal system (#2338)
Co-authored-by: Morb <14136326+Morb0@users.noreply.github.com>

Content.Server/Corvax/StationGoal/StationGoalPaperSystem.cs
@@ -1,5 +1,8 @@
 using System.Linq;
 using Content.Server.Fax;
+using Content.Server.GameTicking.Events;
+using Content.Server.Station.Components;
+using Content.Server.Station.Systems;
 using Content.Shared.Fax.Components;
 using Content.Shared.GameTicking;
 using Content.Shared.Paper;
@@ -14,67 +17,100 @@ namespace Content.Server.Corvax.StationGoal
     /// </summary>
     public sealed class StationGoalPaperSystem : EntitySystem
     {
-        [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+        [Dependency] private readonly IPrototypeManager _proto = default!;
         [Dependency] private readonly IRobustRandom _random = default!;
-        [Dependency] private readonly FaxSystem _faxSystem = default!;
+        [Dependency] private readonly FaxSystem _fax = default!;
         [Dependency] private readonly IPlayerManager _playerManager = default!;
+        [Dependency] private readonly StationSystem _station = default!;
 
         public override void Initialize()
         {
             base.Initialize();
-            SubscribeLocalEvent<RoundStartedEvent>(OnRoundStarted);
+            SubscribeLocalEvent<RoundStartingEvent>(OnRoundStarting);
         }
 
-        private void OnRoundStarted(RoundStartedEvent ev)
+        private void OnRoundStarting(RoundStartingEvent ev)
         {
-            SendRandomGoal();
-        }
-
-        public bool SendRandomGoal()
-        {
-            var availableGoals = _prototypeManager.EnumeratePrototypes<StationGoalPrototype>().ToList();
             var playerCount = _playerManager.PlayerCount;
 
-            var validGoals = availableGoals.Where(goal =>
-                    (!goal.MinPlayers.HasValue || playerCount >= goal.MinPlayers.Value) &&
-                    (!goal.MaxPlayers.HasValue || playerCount <= goal.MaxPlayers.Value)).ToList();
-
-            if (!validGoals.Any())
+            var query = EntityQueryEnumerator<StationGoalComponent>();
+            while (query.MoveNext(out var uid, out var station))
             {
-                return false;
-            }
+                var tempGoals = new List<ProtoId<StationGoalPrototype>>(station.Goals);
+                StationGoalPrototype? selGoal = null;
+                while (tempGoals.Count > 0)
+                {
+                    var goalId = _random.Pick(tempGoals);
+                    var goalProto = _proto.Index(goalId);
 
-            var goal = _random.Pick(validGoals);
-            return SendStationGoal(goal);
+                    if (playerCount > goalProto.MaxPlayers ||
+                        playerCount < goalProto.MinPlayers)
+                    {
+                        tempGoals.Remove(goalId);
+                        continue;
+                    }
+
+                    selGoal = goalProto;
+                    break;
+                }
+
+                if (selGoal is null)
+                    return;
+
+                if (SendStationGoal(uid, selGoal))
+                {
+                    Log.Info($"Goal {selGoal.ID} has been sent to station {MetaData(uid).EntityName}");
+                }
+            }
+        }
+
+        public bool SendStationGoal(EntityUid? ent, ProtoId<StationGoalPrototype> goal)
+        {
+            return SendStationGoal(ent, _proto.Index(goal));
         }
 
         /// <summary>
-        ///     Send a station goal to all faxes which are authorized to receive it.
+        ///     Send a station goal on selected station to all faxes which are authorized to receive it.
         /// </summary>
         /// <returns>True if at least one fax received paper</returns>
-        public bool SendStationGoal(StationGoalPrototype goal)
+        public bool SendStationGoal(EntityUid? ent, StationGoalPrototype goal)
         {
-            var faxes = EntityManager.EntityQuery<FaxMachineComponent>();
+            if (ent is null)
+                return false;
+
+            if (!TryComp<StationDataComponent>(ent, out var stationData))
+                return false;
+
+            var printout = new FaxPrintout(
+                Loc.GetString(goal.Text, ("station", MetaData(ent.Value).EntityName)),
+                Loc.GetString("station-goal-fax-paper-name"),
+                null,
+                null,
+                "paper_stamp-centcom",
+                new List<StampDisplayInfo>
+                {
+                    new() { StampedName = Loc.GetString("stamp-component-stamped-name-centcom"), StampedColor = Color.FromHex("#006600") },
+                });
+
             var wasSent = false;
-            foreach (var fax in faxes)
+            var query = EntityQueryEnumerator<FaxMachineComponent>();
+            while (query.MoveNext(out var faxUid, out var fax))
             {
-                if (!fax.ReceiveStationGoal) continue;
+                if (!fax.ReceiveStationGoal)
+                    continue;
 
-                var printout = new FaxPrintout(
-                    Loc.GetString(goal.Text),
-                    Loc.GetString("station-goal-fax-paper-name"),
-                    null,
-                    null,
-                    "paper_stamp-centcom",
-                    new List<StampDisplayInfo>
+                var largestGrid = _station.GetLargestGrid(stationData);
+                var grid = Transform(faxUid).GridUid;
+                if (grid is not null && largestGrid == grid.Value)
+                {
+                    _fax.Receive(faxUid, printout, null, fax);
+                    foreach (var spawnEnt in goal.Spawns)
                     {
-                        new() { StampedName = Loc.GetString("stamp-component-stamped-name-centcom"), StampedColor = Color.FromHex("#006600") },
-                    });
-                _faxSystem.Receive(fax.Owner, printout, null, fax);
-
-                wasSent = true;
+                        SpawnAtPosition(spawnEnt, Transform(faxUid).Coordinates);
+                    }
+                    wasSent = true;
+                }
             }
-
             return wasSent;
         }
     }

superlowfpss/ss1487

Content.Server/Corvax/StationGoal/StationGoalPaperSystem.cs
@@ -1,5 +1,8 @@
 using System.Linq;
 using Content.Server.Fax;
+using Content.Server.GameTicking.Events;
+using Content.Server.Station.Components;
+using Content.Server.Station.Systems;
 using Content.Server.Paper;
 using Content.Shared.Fax.Components;
 using Content.Shared.GameTicking;
@@ -8,6 +11,7 @@ using Content.Shared.Random.Helpers;
 using Content.Shared.SS220.Photocopier;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
+using Robust.Server.Player;
 
 namespace Content.Server.Corvax.StationGoal
 {
@@ -16,75 +20,111 @@ namespace Content.Server.Corvax.StationGoal
     /// </summary>
     public sealed class StationGoalPaperSystem : EntitySystem
     {
-        [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+        [Dependency] private readonly IPrototypeManager _proto = default!;
         [Dependency] private readonly IRobustRandom _random = default!;
         [Dependency] private readonly FaxSystem _faxSystem = default!;
+        [Dependency] private readonly IPlayerManager _playerManager = default!;
+        [Dependency] private readonly StationSystem _station = default!;
 
         public override void Initialize()
         {
             base.Initialize();
-            SubscribeLocalEvent<RoundStartedEvent>(OnRoundStarted);
+            SubscribeLocalEvent<RoundStartingEvent>(OnRoundStarting);
         }
 
-        private void OnRoundStarted(RoundStartedEvent ev)
+        private void OnRoundStarting(RoundStartingEvent ev)
         {
-            SendRandomGoal();
+            var playerCount = _playerManager.PlayerCount;
+
+            var query = EntityQueryEnumerator<StationGoalComponent>();
+            while (query.MoveNext(out var uid, out var station))
+            {
+                var tempGoals = new List<ProtoId<StationGoalPrototype>>(station.Goals);
+                StationGoalPrototype? selGoal = null;
+                while (tempGoals.Count > 0)
+                {
+                    var goalId = _random.Pick(tempGoals);
+                    var goalProto = _proto.Index(goalId);
+
+                    if (playerCount > goalProto.MaxPlayers ||
+                        playerCount < goalProto.MinPlayers)
+                    {
+                        tempGoals.Remove(goalId);
+                        continue;
+                    }
+
+                    selGoal = goalProto;
+                    break;
+                }
+
+                if (selGoal is null)
+                    return;
+
+                if (SendStationGoal(uid, selGoal))
+                {
+                    Log.Info($"Goal {selGoal.ID} has been sent to station {MetaData(uid).EntityName}");
+                }
+            }
         }
 
-        public bool SendRandomGoal()
+        public bool SendStationGoal(EntityUid? ent, ProtoId<StationGoalPrototype> goal)
         {
-            var availableGoals = _prototypeManager.EnumeratePrototypes<StationGoalPrototype>().ToList();
-            var goal = _random.Pick(availableGoals);
-            return SendStationGoal(goal);
-        }
-        public static string Random_planet_name()
-        {
-            var rand = new Random();
-            string name = $"{(char)rand.Next(65, 90)}-";
-
-            for (var i = 1; i <= 5; i++)
-                name += $"{rand.Next(0, 9)}{(i != 5 ? "-" : "")}";
-
-            return name;
+            return SendStationGoal(ent, _proto.Index(goal));
         }
         /// <summary>
-        ///     Send a station goal to all faxes which are authorized to receive it.
+        ///     Send a station goal on selected station to all faxes which are authorized to receive it.
         /// </summary>
         /// <returns>True if at least one fax received paper</returns>
-        public bool SendStationGoal(StationGoalPrototype goal)
+        public bool SendStationGoal(EntityUid? ent, StationGoalPrototype goal)
         {
-            var faxes = EntityManager.EntityQueryEnumerator<FaxMachineComponent>();
-            var wasSent = false;
-            while (faxes.MoveNext(out var uid, out var fax))
+            if (ent is null)
+                return false;
+
+            if (!TryComp<StationDataComponent>(ent, out var stationData))
+                return false;
+
+            var dataToCopy = new Dictionary<Type, IPhotocopiedComponentData>();
+            var paperDataToCopy = new PaperPhotocopiedData()
             {
-                if (!fax.ReceiveStationGoal) continue;
-
-                var dataToCopy = new Dictionary<Type, IPhotocopiedComponentData>();
-                var paperDataToCopy = new PaperPhotocopiedData()
-                {
-                    Content = Loc.GetString(goal.Text, ("rand_planet_name", Random_planet_name())),
-                    StampState = "paper_stamp-centcom",
-                    StampedBy = new List<StampDisplayInfo>{
-                        new(){
-                            StampedName = Loc.GetString("stamp-component-stamped-name-centcom"),
-                            StampedColor = Color.FromHex("#dca019") //SS220-CentcomFashion-Changed the stamp color
-                        }
+                Content = Loc.GetString(goal.Text, ("station", MetaData(ent.Value).EntityName)),
+                StampState = "paper_stamp-centcom",
+                StampedBy = [
+                    new()
+                    {
+                        StampedName = Loc.GetString("stamp-component-stamped-name-centcom"),
+                        StampedColor = Color.FromHex("#dca019") //SS220-CentcomFashion-Changed the stamp color
                     }
-                };
-                dataToCopy.Add(typeof(PaperComponent), paperDataToCopy);
+                ]
+            };
+            dataToCopy.Add(typeof(PaperComponent), paperDataToCopy);
 
-                var metaData = new PhotocopyableMetaData()
+            var metaData = new PhotocopyableMetaData()
+            {
+                EntityName = Loc.GetString("station-goal-fax-paper-name"),
+                PrototypeId = "PaperNtFormCc"
+            };
+
+            var printout = new FaxPrintout(dataToCopy, metaData);
+
+            var wasSent = false;
+            var query = EntityQueryEnumerator<FaxMachineComponent>();
+            while (query.MoveNext(out var faxUid, out var fax))
+            {
+                if (!fax.ReceiveStationGoal)
+                    continue;
+
+                var largestGrid = _station.GetLargestGrid(stationData);
+                var grid = Transform(faxUid).GridUid;
+                if (grid is not null && largestGrid == grid.Value)
                 {
-                    EntityName = Loc.GetString("station-goal-fax-paper-name"),
-                    PrototypeId = "PaperNtFormCc"
-                };
-
-                var printout = new FaxPrintout(dataToCopy, metaData);
-                _faxSystem.Receive(uid, printout, null, fax);
-
-                wasSent = true;
+                    _faxSystem.Receive(faxUid, printout, null, fax);
+                    foreach (var spawnEnt in goal.Spawns)
+                    {
+                        SpawnAtPosition(spawnEnt, Transform(faxUid).Coordinates);
+                    }
+                    wasSent = true;
+                }
             }
-
             return wasSent;
         }
     }