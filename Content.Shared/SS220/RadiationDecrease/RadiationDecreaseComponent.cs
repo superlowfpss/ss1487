a101102601a8218a7a729620fcf62108d0587608
ReeZer2 <63300653+ReeZer2@users.noreply.github.com>
2024-07-29 02:13:11 +0300

    add: РИТЭГ теперь периодически теряет свой запас радиации (#1465)

---
+++ Content.Shared/SS220/RadiationDecrease/RadiationDecreaseComponent.cs
@@ -0,0 +1,19 @@
+﻿namespace Content.Shared.SS220.RadiationDecrease;
+
+/// <summary>
+///     Decrease radiation per second in Damaged RTG
+/// </summary>
+[RegisterComponent]
+public sealed partial class RadiationDecreaseComponent : Component
+{
+    public readonly int TotalAliveTime = 1200; // 20 minutes
+
+    public readonly TimeSpan CoolDown = TimeSpan.FromSeconds(1f);
+
+    public TimeSpan LastTimeDecreaseRad = TimeSpan.Zero;
+    public TimeSpan LastTimeDecreaseSupply = TimeSpan.Zero;
+
+    public float Intensity = 0; // radiation capacity in RadiationSourceComponent
+    public float Supply = 0; // powersupp in PowerSupplierComponent
+
+}
