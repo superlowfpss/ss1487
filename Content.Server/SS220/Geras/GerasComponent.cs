1598ec366ef1dd2b8804e0517d420ef1a08d14ba
TheArturZh <thearturzh@gmail.com>
2024-08-28 14:26:25 +0300

    Bring geras back

---
+++ Content.Server/SS220/Geras/GerasComponent.cs
@@ -0,0 +1,17 @@
+using Content.Shared.Polymorph;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.SS220.Geras;
+
+/// <summary>
+/// This component assigns the entity with a polymorph action.
+/// </summary>
+[RegisterComponent]
+public sealed partial class GerasComponent : Component
+{
+    [DataField] public ProtoId<PolymorphPrototype> GerasPolymorphId = "SlimeMorphGeras";
+
+    [DataField] public EntProtoId GerasAction = "ActionMorphGeras";
+
+    [DataField] public EntityUid? GerasActionEntity;
+}
