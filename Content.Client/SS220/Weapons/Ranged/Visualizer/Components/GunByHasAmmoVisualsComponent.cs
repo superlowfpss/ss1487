f2833cd5c647d5420e0b8f19346d9be0e8c3ba8c
FunTust <49007663+FunTust@users.noreply.github.com>
2024-09-08 01:53:13 +0300

    Rpg visual fix (#1795)
    * rpg visual fix
    
    * Update GunByHasAmmoVisualizerSystem.cs
    
    * add licence

---
+++ Content.Client/SS220/Weapons/Ranged/Visualizer/Components/GunByHasAmmoVisualsComponent.cs
@@ -0,0 +1,20 @@
+﻿// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+using Content.Shared.Hands.Components;
+
+namespace Content.Client.SS220.Weapons.Ranged.Visualizer.Components;
+
+/// <summary>
+/// Sets which sprite RSI is used for displaying the gun visuals and what state to use based on the ammo count.
+/// </summary>
+[RegisterComponent]
+public sealed partial class GunByHasAmmoVisualsComponent : Component
+{
+    /// <summary>
+    ///     Layer to the sprite of the player that is holding this entity (while the component is toggled on).
+    /// </summary>
+    [DataField("inhandVisuals")]
+    public Dictionary<HandLocation, List<PrototypeLayerData>> InhandVisuals = new();
+
+    [DataField("state")] public string? PreviousState;
+    [ViewVariables] public  int? LayerNumber;
+}
