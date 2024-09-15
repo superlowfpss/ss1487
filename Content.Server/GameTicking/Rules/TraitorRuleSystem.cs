2fcb661fe8765f6cb93c8b613ce7a243bbd15a4a
ReeZer2 <63300653+ReeZer2@users.noreply.github.com>
2024-09-15 17:24:54 +0300

    FIX: Same codewords for all traitors (#1865)
    * FIX: Same codewords for all traitors
    
    * fix commit

--- Content.Server/GameTicking/Rules/TraitorRuleSystem.cs
+++ Content.Server/GameTicking/Rules/TraitorRuleSystem.cs
@@ -36,6 +36,7 @@ public sealed class TraitorRuleSystem : GameRuleSystem<TraitorRuleComponent>
     [Dependency] private readonly SharedRoleSystem _roleSystem = default!;
     [Dependency] private readonly SharedJobSystem _jobs = default!;
     [Dependency] private readonly SharedRoleCodewordSystem _roleCodewordSystem = default!;
+    [Dependency] private readonly GameTicker _gameTicker = default!;
 
     public override void Initialize()
     {
@@ -59,7 +60,21 @@ public sealed class TraitorRuleSystem : GameRuleSystem<TraitorRuleComponent>
 
     private void SetCodewords(TraitorRuleComponent component)
     {
-        component.Codewords = GenerateTraitorCodewords(component);
+        //ss220 same codewords for all traitors start
+        if (!_gameTicker.IsGameRuleAdded<TraitorRuleComponent>())
+        {
+            return;
+        }
+
+        var ruleEnts = _gameTicker.GetAddedGameRules();
+        foreach (var ruleEnt in ruleEnts)
+        {
+            if (TryComp<TraitorRuleComponent>(ruleEnt, out var traitorComp))
+            {
+                component.Codewords = traitorComp.Codewords.Contains(null) ? GenerateTraitorCodewords(component) : traitorComp.Codewords;
+            }
+        }
+        //ss220 same codewords for all traitors end
     }
 
     public string[] GenerateTraitorCodewords(TraitorRuleComponent component)
