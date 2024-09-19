9a86e852483b7ebd7db2cc4926a4a203565d791c
Dexler <69513582+DexlerXD@users.noreply.github.com>
2024-09-19 19:42:05 +0300

    post-merge fixes

--- Content.Shared/Flash/SharedFlashSystem.cs
+++ Content.Shared/Flash/SharedFlashSystem.cs
@@ -9,7 +9,7 @@ public abstract class SharedFlashSystem : EntitySystem
 {
     public ProtoId<StatusEffectPrototype> FlashedKey = "Flashed";
 
-    public virtual void FlashArea(Entity<FlashComponent?> source, EntityUid? user, float range, float duration, float slowTo = 0.8f, bool displayPopup = false, float probability = 1f, SoundSpecifier? sound = null)
+    public virtual void FlashArea(Entity<FlashComponent?> source, EntityUid? user, float range, float duration, float slowTo = 0.8f, bool displayPopup = false, float probability = 1f, SoundSpecifier? sound = null, bool stun = false, float stunDuration = 1f) // 220 flash grenade stun
     {
     }
 }
