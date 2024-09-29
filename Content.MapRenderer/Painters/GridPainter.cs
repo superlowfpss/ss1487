0ba8d9714f4c10990e1158a60c5059f078f93026
Stalen <33173619+stalengd@users.noreply.github.com>
2024-09-29 11:57:57 +0300

    Map rendering crash fix (#1949)
    * Map rendering crash fix
    
    * Returned BOMs

--- Content.MapRenderer/Painters/GridPainter.cs
+++ Content.MapRenderer/Painters/GridPainter.cs
@@ -90,7 +90,14 @@ namespace Content.MapRenderer.Painters
                     var position = transform.LocalPosition;
 
                     var (x, y) = TransformLocalPosition(position, grid);
-                    var data = new EntityData(serverEntity, sprite, x, y);
+                    // SS220 Map Rendering Crash Fix start
+                    //var data = new EntityData(serverEntity, sprite, x, y)
+                    var data = new EntityData(serverEntity, sprite, x, y)
+                    {
+                        MetaData = _sEntityManager.GetComponent<MetaDataComponent>(serverEntity),
+                        LocalPosition = position,
+                    };
+                    // SS220 Map Rendering Crash Fix end
 
                     components.GetOrAdd(transform.GridUid.Value, _ => new List<EntityData>()).Add(data);
                 }
